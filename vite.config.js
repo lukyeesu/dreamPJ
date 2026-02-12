c964d7ec892b50ed1e4519b190e907a243b52872
lukyeesu <lukyee456@hotmail.com>
2026-02-12 19:53:45 +0700

    update something

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // แก้จาก '/dreamPJ/' เป็น '/' เท่านั้น
+  // ใช้ 'loadEnv' หรือเช็กผ่าน mode ก็ได้ แต่ถ้าเอาแบบเข้าใจง่ายและใช้ได้จริง:
+  base: process.env.VERCEL || process.env.NODE_ENV !== 'production' 
+    ? '/' 
+    : '/dreamPJ/',
 })
